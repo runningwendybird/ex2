ee60e7b1bff42bfb4e5db6b752fddecc6680fde6
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 16:24:59 -0800

    finished exercise

--- ex_func.py
+++ ex_func.py
@@ -1,10 +1,11 @@
 # Modify this function to return a list of strings as defined above
 def list_benefits():
-    pass
+    return ["More organized code", "More readable code", 
+    "Easier code reuse", "Allowing programmers to share and connect code together"]
 
 # Modify this function to concatenate to each benefit - " is a benefit of functions!"
 def build_sentence(benefit):
-    pass
+    return benefit + " is a benefit of functions."
 
 def name_the_benefits_of_functions():
     list_of_benefits = list_benefits()
