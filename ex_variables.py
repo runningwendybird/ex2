7dd32ff513482327450158f893abdbcd98bf0186
Hackbright Student <info@hackbrightacademy.com>
2015-01-14 16:14:07 -0800

    Changed formatting of print statements. Looked for 'hello' in string instead of equivalency.

--- ex_variables.py
+++ ex_variables.py
@@ -1,12 +1,12 @@
 
-mystring = "hello"
-myfloat = 10.0
-myint = 20
+mystring = "hello hackbright"
+myfloat = float(10)
+myint = int(20.0)
 
 # testing code
-if mystring == "hello":
+if "hello" in mystring:
     print "String: %s" % mystring
 if isinstance(myfloat, float) and myfloat == 10.0:
-    print "Float: %d" % myfloat
+    print "Float: %f" % myfloat
 if isinstance(myint, int) and myint == 20:
     print "Integer: %d" % myint
